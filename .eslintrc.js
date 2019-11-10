2e75ea839f75c80865caebc2dad9b4d3bdab8f05
Jason Pursey <jason@unumotors.com>
2019-11-10 21:32:04 +0100

    Add style rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,12 @@ module.exports = {
       "es6": true,
       "node": true,
       "jquery": true
+    },
+    "rules": {
+      "comma-spacing": ["error", { "before": false, "after": true }],
+      "comma-style": ["error", "first"],
+      "indent": ["error", 2],
+      "quotes": ["error", "single"],
+      "space-before-function-paren": ["error", "always"],
     }
   };
